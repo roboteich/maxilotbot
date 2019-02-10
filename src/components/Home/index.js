3f3fe44b56012fdd6574c3c938abde600b0857c5
Ross Teichner <rossteichner@gmail.com>
2019-02-10 11:38:41 -0600

    hello world bot

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.scss';
+
+function Home() {
+  return (
+    <section>
+      <h1>Hello World</h1>
+    </section>
+  );
+}
+
+export default Home;
